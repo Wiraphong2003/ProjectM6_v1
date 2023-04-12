10e5910dfcb53ddf365d8f95e06b27ceb3a88922
Wiraphong2003 <111116563+Wiraphong2003@users.noreply.github.com>
2023-04-13 04:33:39 +0700

    as

--- angular-ui/src/app/page/main/main.component.ts
+++ angular-ui/src/app/page/main/main.component.ts
@@ -1,5 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
+import { ServiceService } from 'src/app/service.service';
 
 @Component({
   selector: 'app-main',
@@ -7,11 +9,79 @@ import { FormBuilder } from '@angular/forms';
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent {
-  toppings = this._formBuilder.group({
-    pepperoni: false,
-    extracheese: false,
-    mushroom: false,
-  });
+  data !: any;
+  t1 !: any;
+  t2 !: any;
+  t3 !: any;
+  t4 !: any;
+  t5 !: any;
+  t6 !: any;
+  type !: any;
+  All !: any[];
+  checked = false;
 
-  constructor(private _formBuilder: FormBuilder) { }
+  constructor(
+    private dataService: ServiceService,
+    private http: HttpClient,
+
+  ) {
+
+    http.get(dataService.apiEndpoint + '/Lottary/1').subscribe((data: any) => {
+      console.log(data);
+      this.t1 = data;
+    });
+
+    http.get(dataService.apiEndpoint + '/Lottary/2').subscribe((data: any) => {
+      console.log(data);
+      this.t2 = data;
+    });
+
+
+    http.get(dataService.apiEndpoint + '/Lottary/3').subscribe((data: any) => {
+      console.log(data);
+      this.t3 = data;
+    });
+
+    http.get(dataService.apiEndpoint + '/Lottary/4').subscribe((data: any) => {
+      console.log(data);
+      this.t4 = data;
+    });
+
+    http.get(dataService.apiEndpoint + '/Lottary/5').subscribe((data: any) => {
+      console.log(data);
+      this.t5 = data;
+    });
+
+    http.get(dataService.apiEndpoint + '/read').subscribe((data: any) => {
+      console.log(data);
+      this.data = data;
+    });
+
+  }
+
+  checkbox(lid: any) {
+    // console.log(this.checked);
+    console.log(lid);
+
+    // const result = this.All.some((obj: any) => {
+    //   return obj.id === lid;
+    // });
+
+    // if (result) {
+    //   // 👇️ this runs
+    //   console.log('✅ The value is contained in array');
+    // } else {
+    const s  = this.All.push(lid);
+    console.log(s);
+
+    //   console.log('⛔️ The value is NOT contained in array');
+    // }
+
+    console.log(this.All);
+
+  }
+
+
 }
+
+// https://thankful-blue-bullfrog.cyclic.app/read
