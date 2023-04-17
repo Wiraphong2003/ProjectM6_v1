74867f3e0d8b3a8dc3c567b811003618f9469587
Wiraphong2003 <111116563+Wiraphong2003@users.noreply.github.com>
2023-04-18 04:51:23 +0700

    as

--- angular-ui/src/main.ts
+++ angular-ui/src/main.ts
@@ -1,7 +1,9 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+import './scss/main.scss';
 
 
+import 'font-awesome/scss/font-awesome.scss';
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
